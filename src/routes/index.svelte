e4091b42d10efa103c6aa9725a541db228299801
Peter Chen <manchesterutd7@gmail.com>
2020-03-22 23:12:28 +0400

    localize home page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,7 @@
 <script>
-  export const styles = {
+  import { _ } from 'svelte-i18n';
+
+  const styles = {
     queueNumHeader: 'tc f2 f1-ns ma0',
     queueNumBody: 'tc f1 f-6-ns tracked-mega ma0 green',
   };
@@ -10,6 +12,6 @@
 </svelte:head>
 
 <div>
-  <p class={styles.queueNumHeader}>即時診號</p>
+  <p class={styles.queueNumHeader}>{$_('currentNumber')}</p>
   <p class={styles.queueNumBody}>0000</p>
 </div>
