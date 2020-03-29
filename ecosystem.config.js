6baf9dd488c131516ee7cbbde6f116d52993ee19
Peter Chen <manchesterutd7@gmail.com>
2020-03-29 17:44:46 +0400

    modified pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,13 @@
 module.exports = {
   apps: [{
     name: 'gopher-wood',
-    script: 'npm run dev',
+    script: 'node __sapper__/build',
     env: {
+      PORT: 3000,
       NODE_ENV: 'development',
     },
     env_production: {
+      PORT: 80,
       NODE_ENV: 'production',
     },
   }]
