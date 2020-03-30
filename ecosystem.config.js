a33b803445f61614cb06c0432ce5bdca74e2657f
Ubuntu <ubuntu@ip-172-31-30-121.ap-east-1.compute.internal>
2020-03-30 08:51:21 +0000

    refactored helmet and Nav localization from server

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,11 +3,9 @@ module.exports = {
     name: 'gopher-wood',
     script: 'node __sapper__/build',
     env: {
-      PORT: 3000,
       NODE_ENV: 'development',
     },
     env_production: {
-      PORT: 80,
       NODE_ENV: 'production',
     },
   }]
