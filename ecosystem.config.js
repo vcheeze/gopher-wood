4f982f8c3df131b2e49b0607d907d30e6cc9af70
Peter Chen <manchesterutd7@gmail.com>
2020-03-30 11:11:19 +0400

    updated ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,13 +2,14 @@ module.exports = {
   apps: [{
     name: 'gopher-wood',
     script: 'node __sapper__/build',
+    exec_mode: 'cluster',
+    instances: 1,
+    watch: false,
     env: {
-      PORT: 3000,
       NODE_ENV: 'development',
     },
     env_production: {
-      PORT: 80,
       NODE_ENV: 'production',
     },
   }]
-}
+};
