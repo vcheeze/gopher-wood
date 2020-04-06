ee7046c67f63e2c505038884de613cae5bdd7e29
Yang Cheng <an4976887@gmail.com>
2020-04-06 23:22:29 +0800

    Add status code for updating queue number

--- src/routes/api/updateQueueNumber.js
+++ src/routes/api/updateQueueNumber.js
@@ -4,8 +4,8 @@ export async function get(req, res, next) {
   // check s1 and id
   if (req.query.s1 === 'gopherwoodclinic' && req.query.id === 'g0pherw00dc1inic'){
     global.number = req.query.v1;
-    res.end('Set Queue Number!');
+    return (res.statusCode=200,res.end('Set Queue Number!'));
   } else {
-    res.end('Fail');
+    return (res.statusCode=403,res.end('Fail!'));
   }
 }
