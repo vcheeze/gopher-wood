dbc0b4de621d02d431424b8586046e35eb1a5904
Peter Chen <manchesterutd7@gmail.com>
2020-06-25 09:12:23 +0400

    added firebase anon auth

--- src/routes/api/updateQueueNumber.js
+++ src/routes/api/updateQueueNumber.js
@@ -1,9 +1,21 @@
-import { db } from '../../firebase';
+import { auth, db } from '../../firebase';
 
 export async function get(req, res, next) {
   const { s1, id, v1 } = req.query;
   // check s1 and id
   if (s1 === 'gopherwoodclinic' && id === 'g0pherw00dc1inic') {
+    auth.signInAnonymously().catch(function(err) {
+      var errCode = err.code;
+      var errorMessage = err.message;
+    });
+    auth.onAuthStateChanged(function(user) {
+      if (user) {
+        var isAnonymous = user.isAnonymous;
+        var uid = user.uid;
+      } else {
+        // user is signed out
+      }
+    });
     const queueNumRef = db.ref('queue-number');
     queueNumRef.update({ 'currentNum': +v1 });
     
