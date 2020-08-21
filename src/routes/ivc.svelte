ca74a79c8b6a463bc2fbd3fa58893526172afa87
Peter Chen <manchesterutd7@gmail.com>
2020-08-21 18:28:32 +0400

    added Dialog component to show during IVC process

--- src/routes/ivc.svelte
+++ src/routes/ivc.svelte
@@ -3,7 +3,14 @@
   import { locale, _ } from 'svelte-i18n';
   import Select from 'svelte-select';
   import moment from 'moment';
+  import Spinner from '../components/Spinner.svelte';
+  import Dialog from '../components/Dialog.svelte';
 
+  let showSpinner = false;
+  let showDialog = false,
+    dialogStatus = 'regular',
+    dialogTitle = '',
+    dialogBody = '';
   const today = moment();
   let dateOptions = [];
   let timeOptions = [];
@@ -14,13 +21,20 @@
   $: datePlaceholder = isEnglish ? 'Select a date' : '選擇日期';
   $: timePlaceholder = isEnglish ? 'Select a time' : '選擇時間';
   $: namePlaceholder = isEnglish ? 'Enter your full name' : '姓名';
+  $: successTitle = isEnglish ? 'Your appointment is booked!' : '預約成功！';
+  $: successText = isEnglish ? 'Appointment details' : '預約時段';
+  $: errorTitle = isEnglish ? 'Appointment failed :(' : '預約失敗';
+  $: errorText = isEnglish
+    ? 'Oops, something went wrong. Please try again.'
+    : '發生錯誤，麻煩您再預約一次！';
 
   const styles = {
     title: 'tc',
     form: 'mv2 mh8-ns',
     formGroup: 'pa2 themed',
     label: 'f5',
-    submitButton: 'bg-white ma2 pv2 ph4 fr pointer',
+    action: 'w-100',
+    button: 'primary fr',
   };
 
   // initialize date options
@@ -60,9 +74,8 @@
     );
     if (response.ok) {
       const { data } = await response.json();
-      console.log('==> data: ', data);
+      // console.log('==> data: ', data);
       bookedSlots = data;
-      // TODO show success message
     } else {
       // console.log('==> failed to get booked slots');
       // TODO show error message
@@ -86,7 +99,6 @@
     const slotsForPeriod = bookedSlots.filter(slot =>
       isMorning ? slot.period === 'morning' : slot.period === 'afternoon'
     );
-    console.log('==> slotsForPeriod: ', slotsForPeriod);
     const morning = isEnglish ? 'Morning' : '早上';
     const afternoon = isEnglish ? 'Afternoon' : '下午';
 
@@ -127,6 +139,8 @@
 
   async function onSubmit(e) {
     e.preventDefault();
+    showSpinner = true;
+
     const time = moment(selectedTime.value);
     const date = moment(selectedDate.value);
     const data = {
@@ -135,8 +149,7 @@
       time: time.format('HH:mm:ss'),
       period: time.hour() < 11 ? 'morning' : 'afternoon',
     };
-    console.log('==> data: ', data);
-    debugger;
+    // console.log('==> data: ', data);
     let response = await fetch(`/api/submitIvcAppointment`, {
       method: 'POST',
       headers: {
@@ -146,13 +159,24 @@
     });
     if (response.ok) {
       let json = await response.json();
-      console.log('==> json: ', json);
-      clearFields();
-      // TODO show success message
+      // console.log('==> json: ', json);
+      dialogStatus = 'success';
+      dialogTitle = successTitle;
+      dialogBody = successText;
     } else {
-      console.log('==> failed to book IVC appointment');
-      // TODO show error message
+      // console.log('==> failed to book IVC appointment');
+      dialogStatus = 'error';
+      dialogTitle = errorTitle;
+      dialogBody = errorText;
     }
+
+    showDialog = true;
+    showSpinner = false;
+  }
+
+  function onAcknowledge() {
+    clearFields();
+    showDialog = false;
   }
 
   // onMount(() => {
@@ -168,7 +192,7 @@
 <style>
   .themed {
     --borderRadius: 0;
-    --itemHoverBG: rgb(232, 253, 245);
+    --itemHoverBG: #e8fdf5;
     --placeholderColor: #000;
   }
 
@@ -194,6 +218,17 @@
   <link rel="canonical" href="http://www.gopherwoodclinic.org" />
 </svelte:head>
 
+<Spinner visible={showSpinner} />
+
+<Dialog visible={showDialog} status={dialogStatus}>
+  <p slot="header">{dialogTitle}</p>
+  <p slot="body">{dialogBody}</p>
+  <div class={styles.action} slot="action">
+    <button class={styles.button} on:click={onAcknowledge}>Okay!</button>
+  </div>
+</Dialog>
+<!-- <Banner visible /> -->
+
 <h1 class={styles.title}>{$_('page.ivc.title')}</h1>
 <form class={styles.form}>
   <div class={styles.formGroup}>
@@ -202,7 +237,6 @@
   <div class={styles.formGroup}>
     <!-- <label class={styles.label} for="date">{$_('field.date')}</label> -->
     <Select
-      id="date"
       placeholder={datePlaceholder}
       items={dateOptions}
       bind:selectedValue={selectedDate}
@@ -211,13 +245,12 @@
   <div class={styles.formGroup}>
     <!-- <label class={styles.label} for="time">{$_('field.time')}</label> -->
     <Select
-      id="time"
       placeholder={timePlaceholder}
       items={timeOptions}
       {groupBy}
       bind:selectedValue={selectedTime} />
   </div>
-  <button type="submit" class={styles.submitButton} on:click={onSubmit}>
+  <button type="submit" class={styles.button} on:click={onSubmit}>
     {$_('button.submit')}
   </button>
 </form>
