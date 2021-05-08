1f9e9619292bcc59021a2f18886207e0dce6845b
Peter Chen <peter.wei.chen@pwc.com>
2021-05-08 15:00:44 +0800

    added dotenv config

--- src/db.js
+++ src/db.js
@@ -1,5 +1,21 @@
 import mariadb from 'mariadb';
+import dotenv from 'dotenv';
 
-const pool = mariadb.createPool({ host: 'localhost', port: 3306, user: 'gwuser', password: 'gdubsuperadmin', connectionLimit: 3 });
+dotenv.config();
+const {
+  DB_HOST,
+  DB_PORT,
+  DB_USER,
+  DB_PASSWORD,
+  DB_CONNECTION_LIMIT,
+} = process.env;
+
+const pool = mariadb.createPool({
+  host: DB_HOST,
+  port: DB_PORT,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  connectionLimit: DB_CONNECTION_LIMIT,
+});
 
 export default pool;
